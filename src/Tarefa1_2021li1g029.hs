671362f17ba108d497a2c18120e996a8d92ab0d7
a100900 <a100900@alunos.uminho.pt>
2021-11-11 15:02:41 +0000

    correçao dos erros

--- src/Tarefa1_2021li1g029.hs
+++ src/Tarefa1_2021li1g029.hs
@@ -13,3 +13,33 @@ import LI12122
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
 validaPotencialMapa pecas = undefined
 
+-- determinha o n de elemetos iguais (t1 p2)
+elemiguais :: Peca -> [Peca] -> Int
+elemiguais n [] = 0
+elemiguais n (x:xs) = 
+     if n == x then 1 + elemiguais n xs 
+     else elemiguais n xs
+
+-- verifica se e caixa (t1 p3)
+ecaixa :: (Peca,Coordenadas) -> Bool
+ecaixa (x,_) | x == Caixa = True
+             | otherwise = False
+     
+-- verifica se e bloco (t1 p3)
+ebloco :: (Peca,Coordenadas) -> Bool
+ebloco (x,_) | x == Bloco = True
+             | otherwise = False
+   
+
+-- verifica se a caixa flutua (t1 p3)
+caixaflutua :: [(Peca,Coordenadas)] -> Bool
+caixaflutua [] = False
+
+{-}
+validaPotencialMapa' :: [(Peca, Coordenadas)] -> Bool
+validaPotencialMapa' [] = False 
+validaPotencialMapa'  (x:xs) | snd x == snd (head xs) = False
+                            | (elemiguais n (x:xs)) > 1 = False
+                                                -}
+                                                    
+
