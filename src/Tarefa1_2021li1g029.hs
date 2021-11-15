148b54288b004c49165bee149734d4fa71a9cce9
a100550 <a100550@alunos.uminho.pt>
2021-11-15 10:50:50 +0000

    Juntei as funcoes na principal

--- src/Tarefa1_2021li1g029.hs
+++ src/Tarefa1_2021li1g029.hs
@@ -11,14 +11,22 @@ module Tarefa1_2021li1g029 where
 import LI12122
 
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa pecas = undefined 
+validaPotencialMapa [] = False 
+validaPotencialMapa pecas = posicaoigual pecas == False && nportas pecas <= 1 && caixaflutua pecas == True && espacovazio pecas >= 1 && chao pecas == True 
 
+-- determina se existem mais que uma pecas na mesma posicao (t1 p1)
+posicaoigual :: [(Peca,Coordenadas)] -> Bool 
+posicaoigual [] = False 
+posicaoigual (x:xs) = 
+       if snd x == snd (head xs) then False 
+       else posicaoigual xs
+ 
 -- determinha o n de portas(t1 p2)
-elemiguais :: [(Peca,Coordenadas)] -> Int
-elemiguais [] = 0
-elemiguais (x:xs) = 
-     if fst x == Porta then 1 + elemiguais xs 
-     else elemiguais xs
+nportas :: [(Peca,Coordenadas)] -> Int
+nportas [] = 0
+nportas (x:xs) = 
+     if fst x == Porta then 1 + nportas xs 
+     else nportas xs
 
 -- verifica se e caixa (t1 p3)
 ecaixa :: (Peca,Coordenadas) -> Bool
@@ -29,7 +37,6 @@ ecaixa (x,_) | x == Caixa = True
 ebloco :: (Peca,Coordenadas) -> Bool
 ebloco (x,_) | x == Bloco = True
              | otherwise = False
-   
 
 -- verifica se a caixa flutua (t1 p3)
 caixaflutua :: [(Peca,Coordenadas)] -> Bool
@@ -37,12 +44,14 @@ caixaflutua ((p,(x,y)):xs) =
       if ecaixa (p,(x,y-1)) || ebloco (p,(x,y-1)) then caixaflutua xs
       else False 
 
+-- verifica que existe no min um espaco vazio (t1 p4)
 espacovazio :: [(Peca,Coordenadas)] -> Int 
 espacovazio [] = 0
 espacovazio (x:xs) = 
       if fst x == Vazio then 1 + espacovazio xs 
       else espacovazio xs
 
+-- verifica que a base do mapa e composta por blocos (t1 p5)
 chao :: [(Peca,Coordenadas)] -> Bool 
 chao [] = False
 chao ((p,(a,b)):xs) = 
