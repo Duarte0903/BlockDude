727bcb2f6915d1991fd62f4ca023f259e5f0df50
a100900 <a100900@alunos.uminho.pt>
2021-11-25 14:52:49 +0000

    .

--- src/Tarefa2_2021li1g029.hs
+++ src/Tarefa2_2021li1g029.hs
@@ -16,16 +16,16 @@ import Tarefa1_2021li1g029
 
 constroiMapa :: [(Peca, Coordenadas)] -> Mapa
 constroiMapa pecas = undefined 
-
+{-}
 matrizvazia :: Int -> Int -> [(Peca,Coordenadas)]
 matrizvazia 0 0 = [(Vazio,(0,0))]
 matrizvazia 0 x = sortOn snd ([(Vazio,(x,0))] ++ matrizvazia 0 (x-1))
 matrizvazia y 0 = sortOn snd ([(Vazio,(0,y))] ++ matrizvazia (y-1) 0)
-matrizvazia y x | x >= 0 && y >= 0 = sortOn snd ((matrizvazia y x) ++ (matrizvazia (y-1) (x-1)))
-
+matrizvazia y x | x > 0 && y > 0 = sortOn snd ((matrizvazia y x) ++ (matrizvazia (y-1) (x-1)))
+-}
 
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa mapa = undefined
 
-
+ 
 
