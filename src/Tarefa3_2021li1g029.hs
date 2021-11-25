643eeee7ce1a156901ad647623999ed1ceb48071
a100550 <a100550@alunos.uminho.pt>
2021-11-25 16:04:30 +0000

    tarefa 3

--- src/Tarefa3_2021li1g029.hs
+++ src/Tarefa3_2021li1g029.hs
@@ -10,27 +10,20 @@ module Tarefa3_2021li1g029 where
 
 import LI12122
 
-import Tarefa1_2021li1g029
-
 instance Show Jogo where
-  show = undefined
+  show jogo = aux (0,0) jogo where 
 
-show :: Jogo -> String 
-show (Jogo mapa (Jogador coordenadas direcao temccaixa)) = undefined 
-
-temcaixa :: (Peca,Coordenadas) -> Bool 
-temcaixa (j,(x,y)) = ecaixa (j,(x,y-1))
-
-showjogador :: Jogador -> String
-showjogador (jogador coordenadas direcao temcaixa) = 
-  case direcao of Este -> ">"
-                  Oeste -> "<" 
-  case temcaixa of temcaixa (j,(x,y)) -> "C
-                                          J"
-
-showpeca :: Peca -> String 
-showpeca p = case p of 
-  Vazio -> " "
-  Bloco -> "X"
-  Caixa -> "C"
-  Porta -> "P"             
+aux :: Coordenadas -> Jogo -> String   -- cordenadas -> (mapa jogador)
+aux _ (Jogo [] _) = ""       -- mapa vazio
+aux _ (Jogo [[]] _) = ""
+aux (x,y) (Jogo ([]:t) jogador) = '\n' : aux (x,y+1) (Jogo t jogador)   -- linha vazia
+aux (x,y) (Jogo ((h:hs):t) (Jogador (xj,yj) dir temcaixa)) 
+  | (x,y) == (xj,yj-1) && temcaixa = 'c' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+  | (x,y) == (xj,yj) = if dir == Oeste 
+                       then  '<' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+                       else '>' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+  | h == Bloco = 'X' : mapa
+  | h == Porta = 'P' : mapa
+  | h == Caixa = 'C' : mapa
+  | h == Vazio = ' ' : mapa 
+  where mapa = aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
