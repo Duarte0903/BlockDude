40009333443e482491b1930c3e6e09ab2a75a53a
a100900 <a100900@alunos.uminho.pt>
2021-11-26 16:01:50 +0000

    funçoes auxiliares

--- src/Tarefa4_2021li1g029.hs
+++ src/Tarefa4_2021li1g029.hs
@@ -11,6 +11,7 @@ module Tarefa4_2021li1g029 where
 import LI12122
 import GHCi.Message (THMessage(AddCorePlugin))
 import Data.Text.Unsafe (Iter(Iter))
+import  Tarefa1_2021li1g029
 
 moveJogador :: Jogo -> Movimento -> Jogo         -- jogo = (mapa (coordenadas direcao bool))
 moveJogador (Jogo (mapa) (Jogador (x,y) direcao bool)) m | m == AndarEsquerda = (Jogo (mapa) (Jogador (x,y) Oeste bool))
@@ -19,13 +20,33 @@ moveJogador (Jogo (mapa) (Jogador (x,y) direcao bool)) m | m == AndarEsquerda = 
                                                          | m == Trepar && direcao == Este = (Jogo (mapa) (Jogador (x+1,y+1) Este bool))
                                                          | m == InterageCaixa = (Jogo (mapa) (Jogador (x,y) direcao True))
 
+blocoempilhado :: [(Peca,Coordenadas )] -> Bool 
+blocoempilhado [] = False 
+blocoempilhado ((p,(x,y)):xs) = if ebloco (p,(x,y+1)) then True else False 
+
+
+blocoflutua :: [(Peca, Coordenadas )] -> Bool
+blocoflutua [] = False
+blocoflutua ((p, (x,y)):xs) |ebloco (p, (x,y-1))  = False
+                            |ebloco (p, (x,y+1))  = False 
+                            |ebloco (p, (x-1,y))  = False
+                            |ebloco (p, (x+1,y))  = False 
+                            |ebloco (p, (x+1,y+1))= False 
+                            |ebloco (p, (x+1,y-1))= False
+                            |ebloco (p, (x-1,y+1))= False 
+                            |ebloco (p, (x-1,y-1))= False 
+                            |otherwise = True 
+                            
+
 correrMovimentos :: Jogo -> [Movimento] -> Jogo
-correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao bool)) (m:ms) | m == AndarDireita  && (head ms) == AndarDireita  = correrMovimentos (Jogo (mapa) (Jogador (x+1,y) Este bool)) (tail ms)
-                                                                   | m == AndarEsquerda && (head ms) == AndarEsquerda  = correrMovimentos (Jogo (mapa) (Jogador (x-1,y) Oeste bool)) (tail ms)
-                                                                   | m == Trepar && direcao == Este = correrMovimentos (Jogo (mapa) (Jogador (x+1,y+1) Este bool)) ms
-                                                                   | m == Trepar && direcao == Este = correrMovimentos (Jogo (mapa) (Jogador (x+1,y+1) Este bool)) ms
-                                                                   | m == InterageCaixa = correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao True)) ms
-                                                                   | m == InterageCaixa && (head ms == InterageCaixa ) = correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao False)) (tail ms)
-                                                                   
+correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao bool)) (m:ms) 
+
+ | m == AndarDireita  && (head ms) == AndarDireita  = correrMovimentos (Jogo (mapa) (Jogador (x+1,y) Este bool)) (tail ms)
+ | m == AndarEsquerda && (head ms) == AndarEsquerda  = correrMovimentos (Jogo (mapa) (Jogador (x-1,y) Oeste bool)) (tail ms)
+ | m == Trepar && direcao == Este = correrMovimentos (Jogo (mapa) (Jogador (x+1,y+1) Este bool)) ms
+ | m == Trepar && direcao == Este = correrMovimentos (Jogo (mapa) (Jogador (x+1,y+1) Este bool)) ms
+ | m == InterageCaixa = correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao True)) ms
+ | m == InterageCaixa && (head ms == InterageCaixa ) = correrMovimentos (Jogo (mapa) (Jogador (x,y) direcao False)) (tail ms)
+                                                            
 
 
