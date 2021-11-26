29bd7d8bb0fe8a6a9678d6e51d6ce1cd84bd4e51
a100550 <a100550@alunos.uminho.pt>
2021-11-26 12:22:22 +0000

    tarefa2

--- src/Tarefa2_2021li1g029.hs
+++ src/Tarefa2_2021li1g029.hs
@@ -17,20 +17,21 @@ import Tarefa1_2021li1g029
 constroiMapa :: [(Peca, Coordenadas)] -> Mapa
 constroiMapa pecas = undefined 
 
-matrizvazia :: Int -> Int -> [(Peca,Coordenadas)]
-matrizvazia 0 0 = [(Vazio,(0,0))]
-matrizvazia 0 x = sortOn snd ([(Vazio,(x,0))] ++ matrizvazia 0 (x-1))
-matrizvazia y 0 = sortOn snd ([(Vazio,(0,y))] ++ matrizvazia (y-1) 0)
-matrizvazia y x | x > 0 && y > 0 = sortOn snd ((matrizvazia y x) ++ (matrizvazia (y-1) (x-1))) 
+matrizvazia :: [(Peca, Coordenadas)] -> Mapa
+matrizvazia l = replicate (ymaior l) (replicate (xmaior l) Vazio)
+
+inserepeca :: [(Peca, Coordenadas)] -> Mapa -> Mapa 
+inserepeca [] (y:ys) = (y:ys)
+
                                   
 
-
+{-
 inserePeca :: [(Peca , Coordenadas )] -> [(Peca , Coordenadas )] -> [(Peca , Coordenadas )]
 inserePeca [] [] = []
 inserePeca [] y = y
 inserePeca x [] = x
 inserePeca (x:xs) (y:ys) |snd x == snd y = x : inserePeca xs ys 
-                         |otherwise = y: inserePeca xs ys
+                         |otherwise = y: inserePeca xs ys 
                          
 --insereVazio :: Peca -> [(Peca , Coordenadas )] -> [(Peca , Coordenadas )] 
 --insereVazio p [] = []
@@ -48,6 +49,4 @@ metevazios ((p,(x,y)):t) |p == Vazio =
 
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa mapa = undefined
-
- 
-
+-}
