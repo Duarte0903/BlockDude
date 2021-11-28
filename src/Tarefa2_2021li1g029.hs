17d4e6771674f6871c141f7e5f9d610716a873c9
a100550 <a100550@alunos.uminho.pt>
2021-11-28 17:59:14 +0000

    tarefa2

--- src/Tarefa2_2021li1g029.hs
+++ src/Tarefa2_2021li1g029.hs
@@ -14,50 +14,155 @@ import Data.List
 
 import Tarefa1_2021li1g029
 
+
+{- | Determina o x maior
+
+@
+xmax :: [(Peca,Coordenadas )] -> Int
+xmax [] = 0
+xmax (x:xs) = fst (last (ordena (x:xs))) 
+@
+
+== Exemplos de utlização
+
+>>> xmax [(Bloco,(1,2)),(Bloco,(2,2))]
+2
+
+-}
+xmax :: [(Peca,Coordenadas)] -> Int
+xmax [] = 0
+xmax (x:xs) = fst (last (ordena (x:xs)))
+
+
+{- | Determina o y maior
+
+@
+ymax :: [(Peca,Coordenadas )] -> Int 
+ymax [] = 0
+ymax (x:xs) = snd (last (ordena (x:xs)))
+@
+
+== Exemplos de utilização
+
+>>> xmax [(Bloco,(1,2)),(Bloco,(2,2))]
+2
+
+-}
+ymax :: [(Peca,Coordenadas)] -> Int 
+ymax [] = 0
+ymax (x:xs) = snd (last (ordena (x:xs)))
+
+
+{- | Transforma a lista de peças e coorenadas num mapa
+
+@
 constroiMapa :: [(Peca, Coordenadas)] -> Mapa
-constroiMapa pecas = undefined 
+constroiMapa [] = []
+constroiMapa pecas = aux1 (sort pecas) 0 0 
+@
 
-matrizvazia :: [(Peca, Coordenadas)] -> Mapa
-matrizvazia l = replicate (ymaior l) (replicate (xmaior l) Vazio)
-{-
-matrizporlinhas :: [(Peca, Coordenadas)] -> [[(Peca, Coordenadas)]]
-matrizporlinhas [] = [[]]
-matrizporlinhas x = [x]
-matrizporlinhas (x:xs) = 
-     if snd (snd x) == snd (snd (head xs)) then (x : (head xs)) : tail xs
-     else [x] : xs -}
+== Exemplos de utilização
 
- 
-inserepeca :: [(Peca, Coordenadas)] -> Mapa -> Mapa 
-inserepeca [] (y:ys) = (y:ys)
-inserepeca (x:xs) [] = undefined  
+>>> constroiMapa [(Porta, (0,2)), (Bloco, (0,3)), (Bloco, (1,3)),(Bloco, (2,3)), (Bloco, (3,3)), (Caixa, (4,2)),(Bloco, (4,3)), (Bloco, (5,3)), (Bloco, (6,0)),(Bloco, (6,1)), (Bloco, (6,2)), (Bloco, (6,3))]
+[[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Porta, Vazio, Vazio, Vazio, Caixa, Vazio, Bloco],[Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]]
 
-                                  
-
-{-
-inserePeca :: [(Peca , Coordenadas )] -> [(Peca , Coordenadas )] -> [(Peca , Coordenadas )]
-inserePeca [] [] = []
-inserePeca [] y = y
-inserePeca x [] = x
-inserePeca (x:xs) (y:ys) |snd x == snd y = x : inserePeca xs ys 
-                         |otherwise = y: inserePeca xs ys 
-                         
---insereVazio :: Peca -> [(Peca , Coordenadas )] -> [(Peca , Coordenadas )] 
---insereVazio p [] = []
---insereVazio p   |snd x == snd xs = x : insereVazio p x xs
-                            -- |otherwise = x: insereVazio p x xs
-                            where p = Vazio 
-                                
+-}
+constroiMapa :: [(Peca, Coordenadas)] -> Mapa
+constroiMapa [] = []
+constroiMapa pecas = aux1 (sort pecas) 0 0 
 
 
--- |p/= Bloco && p/= Caixa && p/= Porta = (Vazio,(x,y)) : metevazios t
- -- |otherwise =  (p,(x,y)) : metevazios t
+{- | verifica se o número de linhas é máximo, ou seja, se é igual a (ymax l)
+
+@
+aux1 :: [(Peca, Coordenadas)] -> Int -> Int -> Mapa
+aux1 [] _ _ = [] 
+aux1 l x y | y > ymax l = []
+           | otherwise = aux2 l x y : aux1 l x (y+1)
+@
+
+== Exemplos de utilização 
+
+>>> aux1 [(Porta, (0,2)), (Bloco, (0,3)), (Bloco, (1,3)),(Bloco, (2,3)), (Bloco, (3,3)), (Caixa, (4,2)),(Bloco, (4,3)), (Bloco, (5,3)), (Bloco, (6,0)),(Bloco, (6,1)), (Bloco, (6,2)), (Bloco, (6,3))]
+[(Porta, (0,2)), (Bloco, (0,3)), (Bloco, (1,3)),(Bloco, (2,3)), (Bloco, (3,3)), (Caixa, (4,2)),(Bloco, (4,3)), (Bloco, (5,3)), (Bloco, (6,0)),(Bloco, (6,1)), (Bloco, (6,2)), (Bloco, (6,3))]
+
+-}
+aux1 :: [(Peca, Coordenadas)] -> Int -> Int -> Mapa
+aux1 [] _ _ = [] 
+aux1 l x y | y > ymax l = []
+           | otherwise = aux2 l x y : aux1 l x (y+1)
+
+
+{- | verifica se o número de colunas é maximo, ou seja, se é igual a (xmax l)
+
+@
+aux2 :: [(Peca, Coordenadas)] -> Int -> Int -> [Peca]
+aux2 l x y | x > xmax l = []
+           | otherwise = aux3 l x y : aux2 l (x+1) y 
+           where aux3 :: [(Peca, Coordenadas)] -> Int -> Int -> Peca
+                 aux3 [] _ _ = Vazio 
+                 aux3 ((p,(a,b)):t) x y | x == a && y == b = p 
+                                        | otherwise = aux3 t x y
+@
+
+== Exemplos de utilização
+
+>>> [(Porta, (0,2)), (Bloco, (0,3)), (Bloco, (1,3)),(Bloco, (2,3)), (Bloco, (3,3)), (Caixa, (4,2)),(Bloco, (4,3)), (Bloco, (5,3)), (Bloco, (6,0)),(Bloco, (6,1)), (Bloco, (6,2)), (Bloco, (6,3))]
+[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Bloco]
+
+-}
+aux2 :: [(Peca, Coordenadas)] -> Int -> Int -> [Peca]
+aux2 l x y | x > xmax l = []
+           | otherwise = aux3 l x y : aux2 l (x+1) y 
+           where aux3 :: [(Peca, Coordenadas)] -> Int -> Int -> Peca
+                 aux3 [] _ _ = Vazio 
+                 aux3 ((p,(a,b)):t) x y | x == a && y == b = p 
+                                        | otherwise = aux3 t x y
+
+
+
+{- | Transforma um mapa numa lista de peças e as suas respetivas coordenadas
+
+@
+desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
+desconstroiMapa mapa = auxdes mapa 0 0 
+@
+
+== Exemplos de utilização
+
+>>> desconstroiMapa [[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Porta, Vazio, Vazio, Vazio, Caixa, Vazio, Bloco],[Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]]
+[(Bloco, (6,0)), (Bloco, (6,1)), (Porta, (0,2)), (Caixa, (4,2)),(Bloco, (6,2)), (Bloco, (0,3)), (Bloco, (1,3)), (Bloco, (2,3)),(Bloco, (3,3)), (Bloco, (4,3)), (Bloco, (5,3)), (Bloco, (6,3))]
 
 -}
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
-desconstroiMapa mapa = undefined 
+desconstroiMapa mapa = auxdes mapa 0 0 
 
 
-auxmapa :: Mapa -> Int -> Int -> (Peca, Coordenadas)
-auxmapa (x:xs) 0 0 = (head x, (0,0))
-auxmapa (x:xs) a b = (head x, (a,b)) 
+{- | Recebe um mapa e devolve a lista das peças e das suas coorenadas. O principal
+objetivo é atribuir coordenadas.
+
+@
+auxdes :: Mapa -> Int -> Int -> [(Peca, Coordenadas)]
+auxdes [] _ _ = []
+auxdes (h:t) x y = aux h x y ++ auxdes t (x+1) y 
+             where aux :: [Peca] -> Int -> Int -> [(Peca,Coordenadas)]
+                   aux [] _ _ = []
+                   aux (h:t) x y | h == Vazio = aux t x (y+1)
+                                 | otherwise = (h,(x,y)) : aux t x (y+1)
+@
+
+== Exemplos de utilização 
+
+>>> [[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Porta, Vazio, Vazio, Vazio, Caixa, Vazio, Bloco],[Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]]
+[[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Porta, Vazio, Vazio, Vazio, Caixa, Vazio, Bloco],[Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]]
+
+-}
+auxdes :: Mapa -> Int -> Int -> [(Peca, Coordenadas)]
+auxdes [] _ _ = []
+auxdes (h:t) x y = aux h x y ++ auxdes t (x+1) y 
+             where aux :: [Peca] -> Int -> Int -> [(Peca,Coordenadas)]
+                   aux [] _ _ = []
+                   aux (h:t) x y | h == Vazio = aux t x (y+1)
+                                 | otherwise = (h,(x,y)) : aux t x (y+1)
+
+
