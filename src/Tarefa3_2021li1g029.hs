f8f66d0b7040912f8c5585a38bd53da5f9dc1434
a100550 <a100550@alunos.uminho.pt>
2021-11-28 11:45:11 +0000

    doc3 + doc4

--- src/Tarefa3_2021li1g029.hs
+++ src/Tarefa3_2021li1g029.hs
@@ -10,10 +10,39 @@ module Tarefa3_2021li1g029 where
 
 import LI12122
 
+
+
+{- | Transforma o jogo numa string
+
+@
+aux :: Coordenadas -> Jogo -> String   
+aux _ (Jogo [] _) = ""       
+aux _ (Jogo [[]] _) = ""
+aux (x,y) (Jogo ([]:t) jogador) = '\n' : aux (0,y+1) (Jogo t jogador)  
+aux (x,y) (Jogo ((h:hs):t) (Jogador (xj,yj) dir temcaixa)) 
+  | (x,y) == (xj,yj-1) && temcaixa = 'c' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+  | (x,y) == (xj,yj) = if dir == Oeste 
+                       then '<' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+                      else '>' : aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+  | h == Bloco = 'X' : mapa
+  | h == Porta = 'P' : mapa
+  | h == Caixa = 'C' : mapa
+  | h == Vazio = ' ' : mapa 
+      where mapa = aux (x+1,y) (Jogo (hs:t) (Jogador (xj,yj) dir temcaixa))
+@
+
+== Exemplos de utilização
+
+>>> aux _ (Jogo [] _)
+""
+
+-}
+
+
 instance Show Jogo where
   show jogo = aux (0,0) jogo where
     
-    aux :: Coordenadas -> Jogo -> String   -- cordenadas -> (mapa jogador)
+    aux :: Coordenadas -> Jogo -> String   
     aux _ (Jogo [] _) = ""       -- mapa vazio
     aux _ (Jogo [[]] _) = ""
     aux (x,y) (Jogo ([]:t) jogador) = '\n' : aux (0,y+1) (Jogo t jogador)   -- linha vazia
--- src/Tarefa4_2021li1g029.hs
+++ src/Tarefa4_2021li1g029.hs
@@ -13,30 +13,105 @@ import GHCi.Message (THMessage(AddCorePlugin))
 import Data.Text.Unsafe (Iter(Iter))
 
 
-moveJogador :: Jogo -> Movimento -> Jogo         -- jogo = (mapa (coordenadas direcao bool))
-moveJogador (Jogo (mapa) (Jogador (x,y) dir b)) m | m == AndarEsquerda = (Jogo (mapa) (Jogador (x,y) Oeste b))                                  -- vira para oeste
-                                                  | m == AndarEsquerda && dir == Oeste = (Jogo (mapa) (Jogador (x-1,y) Oeste b))                -- anda para oeste
-                                                  | m == AndarDireita = (Jogo (mapa) (Jogador (x,y) Este b))                                    -- vira para este
-                                                  | m == AndarEsquerda && dir == Oeste = (Jogo (mapa) (Jogador (x+1,y) Este b))                 -- anda para este
-                                                  | m == Trepar && dir == Oeste = (Jogo (mapa) (Jogador (x-1,y+1) Oeste b))                     -- trepa virado para oeste
-                                                  | m == Trepar && dir == Este = (Jogo (mapa) (Jogador (x+1,y+1) Este b))                       -- trepa virado para este
-                                                  | m == InterageCaixa && dir == Este && b == False = (Jogo (mapa) (Jogador (x,y) Este True))   -- pega na caixa virado para este 
-                                                  | m == InterageCaixa && dir == Este && b == True = (Jogo (mapa) (Jogador (x,y) Este False))   -- larga a caixa virado para este
-                                                  | m == InterageCaixa && dir == Oeste && b == False = (Jogo (mapa) (Jogador (x,y) Oeste True)) -- pega na caixa virado para oeste 
-                                                  | m == InterageCaixa && dir == Oeste && b == True = (Jogo (mapa) (Jogador (x,y) Oeste False)) -- larga a caixa virado para oeste 
+{- | Mostra como os diferentes movimentos afetam o jogador
+
+@
+moveJogador :: Jogo -> Movimento -> Jogo
+moveJogador (Jogo mapa (Jogador (x,y) dir b)) m | m == AndarEsquerda = (Jogo mapa (Jogador (x,y) Oeste b))                                  
+                                                | m == AndarEsquerda && dir == Oeste = (Jogo mapa (Jogador (x-1,y) Oeste b))                
+                                                | m == AndarDireita = (Jogo mapa (Jogador (x,y) Este b))                                    
+                                                | m == AndarEsquerda && dir == Oeste = (Jogo mapa (Jogador (x+1,y) Este b))                 
+                                                | m == Trepar && dir == Oeste = (Jogo mapa (Jogador (x-1,y+1) Oeste b))                     
+                                                | m == Trepar && dir == Este = (Jogo mapa (Jogador (x+1,y+1) Este b))                       
+                                                | m == InterageCaixa && dir == Este && b == False = (Jogo mapa (Jogador (x,y) Este True))   
+                                                | m == InterageCaixa && dir == Este && b == True = (Jogo mapa (Jogador (x,y) Este False))   
+                                                | m == InterageCaixa && dir == Oeste && b == False = (Jogo mapa (Jogador (x,y) Oeste True))  
+                                                | m == InterageCaixa && dir == Oeste && b == True = (Jogo mapa (Jogador (x,y) Oeste False))
+@
+
+== Exemplo de utilização
+
+>>> moveJogador (Jogo mapa (Jogador (x,y) dir b)) AndarEsquerda  
+(Jogo mapa (Jogador (x,y) Oeste b))
+
+-}
+moveJogador :: Jogo -> Movimento -> Jogo         -- jogo = (Jogo mapa (Jogador coordenadas direcao bool))
+moveJogador (Jogo mapa (Jogador (x,y) dir b)) m | m == AndarEsquerda = (Jogo mapa (Jogador (x,y) Oeste b))                                  -- vira para oeste
+                                                | m == AndarEsquerda && dir == Oeste = (Jogo mapa (Jogador (x-1,y) Oeste b))                -- anda para oeste
+                                                | m == AndarDireita = (Jogo mapa (Jogador (x,y) Este b))                                    -- vira para este
+                                                | m == AndarEsquerda && dir == Oeste = (Jogo mapa (Jogador (x+1,y) Este b))                 -- anda para este
+                                                | m == Trepar && dir == Oeste = (Jogo mapa (Jogador (x-1,y+1) Oeste b))                     -- trepa virado para oeste
+                                                | m == Trepar && dir == Este = (Jogo mapa (Jogador (x+1,y+1) Este b))                       -- trepa virado para este
+                                                | m == InterageCaixa && dir == Este && b == False = (Jogo mapa (Jogador (x,y) Este True))   -- pega na caixa virado para este 
+                                                | m == InterageCaixa && dir == Este && b == True = (Jogo mapa (Jogador (x,y) Este False))   -- larga a caixa virado para este
+                                                | m == InterageCaixa && dir == Oeste && b == False = (Jogo mapa (Jogador (x,y) Oeste True)) -- pega na caixa virado para oeste 
+                                                | m == InterageCaixa && dir == Oeste && b == True = (Jogo mapa (Jogador (x,y) Oeste False)) -- larga a caixa virado para oeste 
                                                             
+{- | Devolve o jogo final determinado em aux1 depois de correr
+todos os movimentos
 
+@
 correrMovimentos :: Jogo -> [Movimento] -> Jogo
-correrMovimentos (Jogo (mapa) (Jogador (x,y) dir b)) [] = (Jogo (mapa) (Jogador (x,y) Este False))
-correrMovimentos (Jogo (mapa) (Jogador (x,y) dir b)) l = aux1 (Jogo (mapa) (Jogador (x,y) dir b)) l
+correrMovimentos (Jogo mapa (Jogador (x,y) dir b)) [] = Jogo mapa (Jogador (x,y) Este False)
+correrMovimentos (Jogo mapa (Jogador (x,y) dir b)) l = aux1 (Jogo mapa (Jogador (x,y) dir b)) l
+@
 
+-}
+correrMovimentos :: Jogo -> [Movimento] -> Jogo
+correrMovimentos (Jogo mapa (Jogador (x,y) dir b)) [] = Jogo mapa (Jogador (x,y) Este False)
+correrMovimentos (Jogo mapa (Jogador (x,y) dir b)) l = aux1 (Jogo mapa (Jogador (x,y) dir b)) l
+
+{- | Recebe um jogo e uma lista de movimentos. 
+Devolve um jogo onde foram corridos todos os movimentos
+
+@
 aux1 :: Jogo -> [Movimento] -> Jogo
-aux1 (Jogo (mapa) (Jogador (x,y) dir b)) l = aux3 (aux2 (Jogo (mapa) (Jogador (x,y) dir b)) l)
+aux1 (Jogo mapa (Jogador (x,y) dir b)) l = aux3 (aux2 (Jogo mapa (Jogador (x,y) dir b)) l)
+@
 
+-}
+aux1 :: Jogo -> [Movimento] -> Jogo
+aux1 (Jogo mapa (Jogador (x,y) dir b)) l = aux3 (aux2 (Jogo mapa (Jogador (x,y) dir b)) l)
+
+{- | Recebe um jogo e uma lista de movimentos. Devolve uma lista de jogos, onde cada um corresponde
+a correr um movimento no jogo original
+
+@
 aux2 :: Jogo -> [Movimento] -> [Jogo]
-aux2 (Jogo (mapa) (Jogador (x,y) dir b)) [] = [(Jogo (mapa) (Jogador (x,y) dir b))]
-aux2 (Jogo (mapa) (Jogador (x,y) dir b)) (h:t) = moveJogador (Jogo (mapa) (Jogador (x,y) dir b)) h : aux2 (Jogo (mapa) (Jogador (x,y) dir b)) t
+aux2 (Jogo mapa (Jogador (x,y) dir b)) [] = [Jogo mapa (Jogador (x,y) dir b)]
+aux2 (Jogo mapa (Jogador (x,y) dir b)) (h:t) = moveJogador (Jogo mapa (Jogador (x,y) dir b)) h : aux2 (Jogo mapa (Jogador (x,y) dir b)) t
+@
 
+== Exemplos de utilização
+
+>>> aux2 (Jogo mapa (Jogador (x,y) dir b)) []  
+[Jogo mapa (Jogador (x,y) dir b)]
+
+>>> aux2 (Jogo mapa (Jogador (x,y) dir b)) (h:t)  
+moveJogador (Jogo mapa (Jogador (x,y) dir b)) h : aux2 (Jogo mapa (Jogador (x,y) dir b)) t
+
+-}
+aux2 :: Jogo -> [Movimento] -> [Jogo]
+aux2 (Jogo mapa (Jogador (x,y) dir b)) [] = [Jogo mapa (Jogador (x,y) dir b)]
+aux2 (Jogo mapa (Jogador (x,y) dir b)) (h:t) = moveJogador (Jogo mapa (Jogador (x,y) dir b)) h : aux2 (Jogo mapa (Jogador (x,y) dir b)) t
+
+{- | Transforma uma lista de jogos num jogo
+
+@
 aux3 :: [Jogo] -> Jogo 
-aux3 [(Jogo (mapa) (Jogador (x,y) dir b))] = (Jogo (mapa) (Jogador (x,y) dir b))
+aux3 [Jogo mapa (Jogador (x,y) dir b)] = Jogo mapa (Jogador (x,y) dir b)
+aux3 (h:t) = aux3 t
+@
+
+== Exemplos de utilização
+
+>>> aux3 [Jogo mapa (Jogador (x,y) dir b)]
+Jogo mapa (Jogador (x,y) dir b)
+
+>>> aux3 (h:t) 
+aux3 t
+
+-}
+aux3 :: [Jogo] -> Jogo 
+aux3 [Jogo mapa (Jogador (x,y) dir b)] = Jogo mapa (Jogador (x,y) dir b)
 aux3 (h:t) = aux3 t
