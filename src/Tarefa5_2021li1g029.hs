4d0f95615e080df38d34d153d8ed1a50626be295
a100550 <a100550@alunos.uminho.pt>
2021-12-08 17:33:25 +0000

    t5+t6

---
+++ src/Tarefa5_2021li1g029.hs
@@ -0,0 +1,11 @@
+{- |
+Module      : Tarefa5_2021li1g029
+Description : Aplicação Gráfica
+Copyright   : Duarte Leitão <a100550@alunos.uminho.pt>;
+            : João Pereira <a100900@alunos.uminho.pt>;
+
+Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
+-}
+module Main where
+
+main = undefined
---
+++ src/Tarefa6_2021li1g029.hs
@@ -0,0 +1,14 @@
+{- |
+Module      : Tarefa6_2021li1g029
+Description : Resolução de um puzzle
+Copyright   : Duarte Leitão <a100550@alunos.uminho.pt>;
+            : João Pereira <a100900@alunos.uminho.pt>;
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
+-}
+module Tarefa6_2021li1gXXX where
+
+import LI12122
+
+resolveJogo :: Int -> Jogo -> Maybe [Movimento]
+resolveJogo i jogo = undefined
