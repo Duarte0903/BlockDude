53e59d70eff2183f5940b0797e107939d3c6476f
a100550 <a100550@alunos.uminho.pt>
2021-12-10 13:33:20 +0000

    menu

--- src/Tarefa5_2021li1g029.hs
+++ src/Tarefa5_2021li1g029.hs
@@ -8,4 +8,23 @@ Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
 -}
 module Main where
 
-main = undefined
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Juicy(loadJuicy)
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Data.Display
+
+texto :: Picture
+texto = translate 200 200 (Text "Jogar") 
+
+window :: Display 
+window = InWindow
+  "Janela de Exemplo" -- Nome da Janela
+  (1366,768)           -- Dimensão da Janela
+  (683,384)             -- Posição no ecrã 
+
+background :: Color 
+background = white
+
+main :: IO ()
+main = do display window background texto
