e4f8ebdcf79f7212efda5055eb13713b09979bcc
a100550 <a100550@alunos.uminho.pt>
2021-12-11 16:03:51 +0000

    inicio menu

--- src/Tarefa5_2021li1g029.hs
+++ src/Tarefa5_2021li1g029.hs
@@ -14,17 +14,59 @@ import Graphics.Gloss.Juicy(loadJuicy)
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Display
 
-texto :: Picture
-texto = translate 200 200 (Text "Jogar") 
+data Jogo = Jogo (Int,Int) [(Int,Int)]
+
+data Menu = Controlador Opcoes | Modojogo Jogo | VenceuJogo 
+ 
+data Opcoes = Jogar | Sair 
+
+type World = (Menu,Jogo)
+
 
 window :: Display 
 window = InWindow
-  "Janela de Exemplo" -- Nome da Janela
+  "Block Dude"         -- Nome da Janela
   (1366,768)           -- Dimensão da Janela
-  (683,384)             -- Posição no ecrã 
+  (683,384)            -- Posição no ecrã 
 
-background :: Color 
-background = white
+
+fr :: Int              -- Frame Rate
+fr = 25
+
+
+draw :: World -> Picture 
+draw (VenceuJogo, jogo) = Translate (-200) 0 (color red (Text "Vitória"))
+draw (Controlador Jogar, jogo) = Pictures [Color blue $ drawOption "Jogar", Translate (20) (-90) $ drawOption "Sair", Translate (-310) (100) $ color orange   $ Text "Block Dude"]
+draw (Controlador Sair, jogo) = Pictures [drawOption "Jogar", Color blue $ Translate (20) (-90) $ drawOption "Sair", Translate (-310) (100) $ color orange $ Text "Block Dude"]
+draw (Modojogo (Jogo (x,y) l), jogo) = undefined 
+
+
+drawOption :: String -> Picture
+drawOption option = Translate (-50) 0 $ Scale (0.5) (0.5) $ Text option
+
+
+engine :: (Int,Int) -> [(Int,Int)] -> Jogo
+engine p l = Jogo p (filter (p/=) l)
+
+
+event :: Event -> World -> World
+event _ w = w
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar, jogo) = (Modojogo jogo, jogo)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar, jogo) = (Controlador Sair, jogo)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar, jogo) = (Controlador Sair, jogo)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair, jogo) = (Controlador Jogar, jogo)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair, jogo) = (Controlador Jogar, jogo)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair, jogo) = undefined
+
+
+time :: Float -> World -> World
+time _ w = w 
+
+
+estado :: World
+estado = (Controlador Jogar, Jogo (200, 100) [(50, 50), (-250, -100), (-100, -50)])
+
 
 main :: IO ()
-main = do display window background texto
+main = do 
+  play window white fr estado draw event time 
