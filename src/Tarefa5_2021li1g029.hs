bb0c3b36523f24dd05dfef74c22e75623e57c3f3
a100550 <a100550@alunos.uminho.pt>
2021-12-16 11:19:21 +0000

    background

--- src/Tarefa5_2021li1g029.hs
+++ src/Tarefa5_2021li1g029.hs
@@ -23,7 +23,11 @@ data Opcoes = Jogar | Creditos | Sair
 
 data Cred = VMenu
 
-type World = (Menu,Jogo)
+data Imagens = Imagens { 
+  background :: Picture
+  }
+
+type World = (Menu,Jogo,Imagens)
 
 type Creditos = String 
 
@@ -39,12 +43,12 @@ fr = 25
 
 
 draw :: World -> Picture 
-draw (VenceuJogo, jogo) = Translate (-200) 0 (color red (Text "VENCEU!"))
-draw (Controlador Jogar, jogo) = Pictures [Color blue $ drawOption "Jogar", Translate (20) (-90) $ drawOption "Sair", Translate (-35) (-165) $ drawOption "Creditos", Translate (-310) (100) $ color orange   $ Text "Block Dude"]
-draw (Controlador Creditos, jogo) = Pictures [drawOption "Jogar", Translate (20) (-90) $ drawOption "Sair", Translate (-35) (-165) $ Color blue $ drawOption "Creditos", Translate (-310) (100) $ color orange   $ Text "Block Dude"]
-draw (Controlador Sair, jogo) = Pictures [drawOption "Jogar", Color blue $ Translate (20) (-90) $ drawOption "Sair",Translate (-35) (-165) $ drawOption "Creditos", Translate (-310) (100) $ color orange $ Text "Block Dude"]
-draw (Modojogo (Jogo (x,y) l), jogo) = undefined
-draw (Modocred VMenu, jogo) = Pictures drawCredits
+draw (VenceuJogo, jogo, imgs) = Translate (-200) 0 (color red (Text "VENCEU!"))
+draw (Controlador Jogar, jogo, imgs) = Pictures [ background imgs, Color blue $ drawOption "Jogar", Translate (20) (-90) $ drawOption "Sair", Translate (-35) (-165) $ drawOption "Creditos", Translate (-310) (100) $ color orange   $ Text "Block Dude"]
+draw (Controlador Creditos, jogo, imgs) = Pictures [drawOption "Jogar", Translate (20) (-90) $ drawOption "Sair", Translate (-35) (-165) $ Color blue $ drawOption "Creditos", Translate (-310) (100) $ color orange   $ Text "Block Dude"]
+draw (Controlador Sair, jogo, imgs) = Pictures [drawOption "Jogar", Color blue $ Translate (20) (-90) $ drawOption "Sair",Translate (-35) (-165) $ drawOption "Creditos", Translate (-310) (100) $ color orange $ Text "Block Dude"]
+draw (Modojogo (Jogo (x,y) l), jogo, imgs) = undefined
+draw (Modocred VMenu, jogo, imgs) = Pictures drawCredits
 
 drawCredits :: [Picture]   -- Desenha a página dos Créditos 
 drawCredits = [Translate (-155) 0 $ Scale (0.5) (0.5) $ Color blue $ Text "Duarte Leitao", Translate (-155) (-70) $ Scale (0.5) (0.5) $ Color blue $ Color blue $ Text "Joao Pereira", Translate (-25) (-300) $ Scale (0.3) (0.3) $ Color red $ Color orange $ Text "Menu"]
@@ -59,22 +63,22 @@ engine p l = Jogo p (filter (p/=) l)
 
 
 event :: Event -> World -> World
-event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar, jogo) = (Modojogo jogo, jogo)
-event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar, jogo) = (Controlador Creditos, jogo)
-event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar, jogo) = (Controlador Sair, jogo)
-event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair, jogo) = (Controlador Jogar, jogo)
-event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair, jogo) = (Controlador Creditos, jogo)
-event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair, jogo) = undefined
-event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Creditos, jogo) = (Controlador Jogar, jogo)
-event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Creditos, jogo) = (Controlador Sair, jogo)
-event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Creditos, jogo) = (Modocred VMenu, jogo)
-event (EventKey (SpecialKey KeyEnter) Down _ _ ) (Modocred VMenu, jogo) = (Controlador Jogar, jogo)
-event (EventKey (SpecialKey KeyEnter) Down _ _) (VenceuJogo, jogo) = (Controlador Jogar, jogo)
-event _ (Modojogo (Jogo (x, y) []), jogo) = (VenceuJogo, jogo)
-event (EventKey (SpecialKey KeyUp) Down _ _) (Modojogo (Jogo (x, y) l), jogo) = (Modojogo $ engine (x + 50, y + 50) l, jogo)
-event (EventKey (SpecialKey KeyDown) Down _ _) (Modojogo (Jogo (x, y) l), jogo) = (Modojogo $ engine (x -50, y - 50) l, jogo)
-event (EventKey (SpecialKey KeyLeft) Down _ _) (Modojogo (Jogo (x, y) l), jogo) = (Modojogo $ engine (x - 50, y) l, jogo)
-event (EventKey (SpecialKey KeyRight) Down _ _) (Modojogo (Jogo (x, y) l), jogo) = (Modojogo $ engine (x + 50, y) l, jogo)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Jogar, jogo, imgs) = (Modojogo jogo, jogo, imgs)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Jogar, jogo, imgs) = (Controlador Creditos, jogo, imgs)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Jogar, jogo, imgs) = (Controlador Sair, jogo, imgs)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Sair, jogo, imgs) = (Controlador Jogar, jogo, imgs)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Sair, jogo, imgs) = (Controlador Creditos, jogo, imgs)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Sair, jogo, imgs) = undefined
+event (EventKey (SpecialKey KeyDown) Down _ _) (Controlador Creditos, jogo, imgs) = (Controlador Jogar, jogo, imgs)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Controlador Creditos, jogo, imgs) = (Controlador Sair, jogo, imgs)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (Controlador Creditos, jogo, imgs) = (Modocred VMenu, jogo, imgs)
+event (EventKey (SpecialKey KeyEnter) Down _ _ ) (Modocred VMenu, jogo, imgs) = (Controlador Jogar, jogo, imgs)
+event (EventKey (SpecialKey KeyEnter) Down _ _) (VenceuJogo, jogo, imgs) = (Controlador Jogar, jogo, imgs)
+event _ (Modojogo (Jogo (x, y) []), jogo, imgs) = (VenceuJogo, jogo, imgs)
+event (EventKey (SpecialKey KeyUp) Down _ _) (Modojogo (Jogo (x, y) l), jogo, imgs) = (Modojogo $ engine (x + 50, y + 50) l, jogo, imgs)
+event (EventKey (SpecialKey KeyDown) Down _ _) (Modojogo (Jogo (x, y) l), jogo, imgs) = (Modojogo $ engine (x -50, y - 50) l, jogo, imgs)
+event (EventKey (SpecialKey KeyLeft) Down _ _) (Modojogo (Jogo (x, y) l), jogo, imgs) = (Modojogo $ engine (x - 50, y) l, jogo, imgs)
+event (EventKey (SpecialKey KeyRight) Down _ _) (Modojogo (Jogo (x, y) l), jogo, imgs) = (Modojogo $ engine (x + 50, y) l, jogo, imgs)
 event _ w = w
 
 
@@ -83,10 +87,14 @@ time :: Float -> World -> World
 time _ w = w 
 
 
-estado :: World
-estado = (Controlador Jogar, Jogo (200, 100) [(50, 50), (-250, -100), (-100, -50)])
+loadImages :: IO Imagens
+loadImages = do
+   backim <- loadBMP "background.bmp"
+   return (Imagens backim)
 
 
 main :: IO ()
-main = do 
+main = do
+  imagens <- loadImages 
+  let estado = (Controlador Jogar, Jogo (200, 100) [(50, 50), (-250, -100), (-100, -50)], imagens)
   play window white fr estado draw event time 
