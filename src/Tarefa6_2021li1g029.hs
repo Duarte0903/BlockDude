54bd4d2af0d7b9b488518eb89fb0abc6d96a32c9
a100550 <a100550@alunos.uminho.pt>
2022-01-03 17:19:44 +0000

    .......

--- src/Tarefa6_2021li1g029.hs
+++ src/Tarefa6_2021li1g029.hs
@@ -6,7 +6,7 @@ Copyright   : Duarte Leitão <a100550@alunos.uminho.pt>;
 
 Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
 -}
-module Tarefa6_2021li1gXXX where
+module Tarefa6_2021li1g029 where
 
 import LI12122
 
